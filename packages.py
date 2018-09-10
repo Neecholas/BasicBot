4fbf0e9aed3d1661f9f8a9258b90a1caa3bf0b5a
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-09-10 23:14:32 +0100

    Added the directory of the corpus for the chatbot

--- packages.py
+++ packages.py
@@ -1,2 +1,8 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+import os
+
+bot = ChatBot('Basic Bot')
+#creates an instance of the chatbot with 'basic bot' as it's name
+bot.set_trainer(ListTrainer)
+for files in os.listdir('home/nick/.local/lib/python3.5/site-packages/chatterbot_corpus')
